2a940a1c721088dbabf04149bda2916d69b1d012
Patience <opoku817@morrisville.edu>
2020-09-22 21:50:06 -0400

    first commit

---
+++ feedback.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<meta name= "description" content ="TThis program is to allow the user access country information including
+the country's name, flag image, the population and region using a
+input or text field as the entry point in order to reveal the preferred request.">
+<head>
+	<link rel="stylesheet" type="text/css" href="maindesign.css">
+</head>
+
+	<body>
+<div id="wrapper">	
+	<header>
+		<h1>
+			Your Next Vacation Finder
+		</h1>
+	</header>
+	<nav>
+		<ul>
+			<br><br>
+			<a href="./index.html">Home</a>
+			<br><br><br>
+			<a href="./cf.html">Finder</a>
+			<br><br><br>
+			<a href="./feedback.html">FeedBack</a>
+			<br><br><br>
+			<a href="./credits.html">Credits</a>
+		</ul>	
+	</nav>
+	
+<h2><p> "Without continual growth and progress,
+ such words as improvement, achievement, and success have no meaning." by Benjamin Franklin </p></h2>	
+ <h2> Select your form of </h2>
+<select size="1" name="socialmedia" id="contacts">
+	<option> Not Applicable </option>
+	<option> SnapChat </option>
+	<option> Facebook </option>
+	<option> Instagram </option>
+	<option> LinkedIn </option>
+	</select>
+	<br><br>
